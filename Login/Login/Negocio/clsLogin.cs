f27971d9d7d4635da2fa8b6b5218fa0722672dbc
William Diaz <williamefrain1994@gmail.com>
2020-10-10 19:15:33 -0600

    comentariando codigo

--- Login/Login/Negocio/clsLogin.cs
+++ Login/Login/Negocio/clsLogin.cs
@@ -8,15 +8,15 @@ namespace Login.Negocio
     class clsLogin
     {
         public bool verificar(login l) {
-            if (l.User.Equals(l.UserComparer) && l.Pass.Equals(l.PassComparer))
-            {
-                return true;
-            }
-            else {
-                return false;
-            }
+            //if (l.User.Equals(l.UserComparer) && l.Pass.Equals(l.PassComparer))
+            //{
+            //    return true;
+            //}
+            //else {
+            //    return false;
+            //}
 
-            
+            return false;
         }
     }
 }
